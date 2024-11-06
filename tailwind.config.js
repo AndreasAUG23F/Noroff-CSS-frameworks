6a51878d38830331c3fcf1199db93a2275441a2e
AndreasAUG23F <andabr00198@stud.noroff.no>
2024-11-06 21:14:49 +0100

    added changes to tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 export default {
   content: ["./**/*.{html,js,ts}", "!./node_modules/**/*"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#6d3feb",
+        secondary: "#6b39eb",
+        accent: "#00e676",
+        danger: "#ff1744",
+        dark: "#2e2c2c",
+        grayish: "#f8f8f8",
+      },
+      boxShadow: {
+        "custom-lg": "0 10px 15px rgba(0, 0, 0, 0.3)",
+      },
+      fontFamily: {
+        sans: ["Roboto", "sans-serif"],
+      },
+      screens: {
+        xs: "480px",
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/forms")],
 };
